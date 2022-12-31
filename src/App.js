16d623a0110fbf487fd1c9c2a03274f3eac27b7a
Mustafa <mustafaenisek@gmail.com>
2022-12-31 12:46:55 +0300

    First Push

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const key = "79a3a5039e496721d7bfbf3f4cb7f5c7";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=metric&lang=TR`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Konum Gir"
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].description}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°C</p>
+              ) : null}
+              <p>Felt Temperature</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} km/sa</p>
+              ) : null}
+              <p>Wind</p>
+            </div>
+          </div>
+        )}
+        <div className="footer">
+          <p>Weather_App</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
